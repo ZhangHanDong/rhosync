c44f966651b3bdf9631bb0e19774dd0181eafbe5
adamblum <a@adamblum.com>
2008-09-13 11:54:50 -0700

    generic code for Source update, create, delete

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -3,6 +3,16 @@ require 'digest/md5'
 
 class SourcesController < ApplicationController
 
+  # helper function to come up with the string used for the hash
+  # avhash has jasj
+  def hashstring(h)
+    result="{"
+    h.keys.each do |x|
+      result << (x +"=>" + h[x] + ",")
+    end
+    result=result[0...size-1] # chop off the last comma!
+    result += "}"
+  end
 
   # this connect to the web service of the given source backend and:
   # - does a prolog (generally logging in)
@@ -16,9 +26,61 @@ class SourcesController < ApplicationController
     # make sure to use client and session_id variables
     # in your code that is edited into each source!
     callbinding=eval(@source.prolog+";binding")
-    callbinding=eval(@source.updatecall+";binding",callbinding) if @source.updatecall
-    callbinding=eval(@source.call+";"+ @source.epilog+ ";binding",callbinding)
-    #result=eval("result",callbinding  - THIS IS TO JUST GET DATA RESULTS
+
+    # first do all the the creates
+    if @source.createcall and @source.createcall.size>0
+      objvals=ObjectValue.find_by_update_type("create")
+      createobjects=objvals.map {|x| x.object }
+      createobjects=createobjects.uniq
+      # createobjects is all the distinct objects that are marked for create
+      createobjects.each do |x|
+        objvals=ObjectValue.find_by_object(x)  # this has all the attribute value pairs now
+        attrvalues={}
+        objvals.each do |x|
+          attrvalues[x.attribute]=x.value
+        end
+        # now attrvalues has the attribute values needed for the createcall
+        avstring=hashstring(attrvalues)
+        callbinding=eval("attrvals="+avstring+";"+@source.createcall+";binding",callbinding)
+      end
+    end
+
+    # now do the updates
+    if @source.updatecall and @source.updatecall.size>0
+      objvals=ObjectValue.find_by_update_type("update")
+      updateobjects=objvals.map {|x| x.object }
+      updateobjects=updateobjects.uniq
+      updateobjects.each do |x|
+        objvals=ObjectValue.find_by_object(x)  # this has all the attribute value pairs now
+        attrvalues={}
+        objvals.each do |x|
+          attrvalues[x.attribute]=x.value
+        end
+        # now attrvalues has the attribute values needed for the createcall
+        avstring=hashstring(attrvalues)
+        callbinding=eval("attrvals="+avstring+";"+@source.updatecall+";binding",callbinding)
+      end
+    end
+
+
+    # now do the deletes
+    if @source.updatecall and @source.updatecall.size>0
+      objvals=ObjectValue.find_by_update_type("delete")
+      deleteobjects=objvals.map {|x| x.object }
+      deleteobjects.each do |x|
+        callbinding=eval("deleteobj="+x+";"+@source.deletecall+";binding",callbinding)
+      end
+    end
+
+    if @source.call
+      # now do the query call
+      callbinding=eval(@source.call+";binding",callbinding)
+      # now take apart the returned data 
+      callbinding=eval(@source.sync,callbinding)
+    end
+    
+    # now do the logoff
+    callbinding=eval(@source.epilog+ ";binding",callbinding) if @source.epilog
     eval(@source.sync,callbinding)
     redirect_to :controller=>"sources",:action=>"show"
   end
