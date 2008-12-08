e5a95b1a4c1391dbb755fe37b61e34bebabdfbbf
adamblum <a@adamblum.com>
2008-12-07 17:48:14 -0800

    no need for activesalesforce in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -70,6 +70,5 @@ Rails::Initializer.run do |config|
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
-  require 'activesalesforce'
 
 end
