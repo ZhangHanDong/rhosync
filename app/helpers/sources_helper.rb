d5f029b04d96a1ee9a309d94a92cf2895dc82658
adamblum <a@adamblum.com>
2008-12-09 16:38:25 -0800

    the syncing now works if from a named Source Adapter class.  just put the Ruby file in your lib directory and then reference the source adapter class name in your source. it will then use your defined class.
    BE SURE TO HAVE QUERY, CREATE, UPDATE, DELETE, LOGIN AND LOGOFF methods in the class. we'll have a generator for these classes soon enough

--- app/helpers/sources_helper.rb
+++ app/helpers/sources_helper.rb
@@ -17,90 +17,103 @@ module SourcesHelper
 
   def do_refresh(id)
     @source=Source.find id
+    @source.initadapter
     # not all endpoints require WSDL! dont do this if you dont see WSDL in the URL (a bit of a hack)
-    client = SOAP::WSDLDriverFactory.new(@source.url).create_rpc_driver if @source.url and @source.url.size>0 and @source.url=~/wsdl/
-    # make sure to use client and session_id variables
+    @source.source_adapter.client = SOAP::WSDLDriverFactory.new(@source.url).create_rpc_driver if @source.url and @source.url.size>0 and @source.url=~/wsdl/
+    # make sure to use @client and @session_id variables
     # in your code that is edited into each source!
-    callbinding=eval %"#{@source.prolog};binding"
+    if @source.source_adapter
+      @source.source_adapter.login
+    else
+      callbinding=eval %"#{@source.prolog};binding"
+    end
 
     # first do all the the creates
-    if @source.createcall and @source.createcall.size>0
-      creates=ObjectValue.find_by_sql("select * from object_values where update_type='create' and source_id="+id)
-      uniqobjs=creates.map {|x| x.object}
-      uniqobjs.uniq!
-      uniqobjs.each do |x|
-        p "Searching for attribute values for object: "+x
-        xvals=ObjectValue.find_all_by_object_and_update_type(x,'create')  # this has all the attribute value pairs for this particular object
-        if xvals.size>0
-          attrvalues={}
-          xvals.each do |y|
-            p "Attribute: " + y.attrib
-            p "Value: " + y.value
-            attrvalues[y.attrib]=y.value if y.attrib and y.value
-            y.destroy
-          end
-          # now attrvalues has the attribute values needed for the createcall
-          # the Sugar adapter will use the name_value_list variable that we're building up here
-          # TODO: name_value_list is probably too specific to Sugar
-          #  need a clean way to pass the attrvalues hash to any source adapter cleanly
-          p "Attributes hash size: " + attrvalues.size.to_s
-          nvlist=make_name_value_list(attrvalues)
-          p "nvlist " + nvlist
+    creates=ObjectValue.find_by_sql("select * from object_values where update_type='create' and source_id="+id)
+    uniqobjs=creates.map {|x| x.object}
+    uniqobjs.uniq!
+    uniqobjs.each do |x|
+      xvals=ObjectValue.find_all_by_object_and_update_type(x,'create')  # this has all the attribute value pairs for this particular object
+      if xvals.size>0
+        attrvalues={}
+        xvals.each do |y|
+          attrvalues[y.attrib]=y.value if y.attrib and y.value
+          y.destroy
+        end
+        # now attrvalues has the attribute values needed for the createcall
+        # the Sugar adapter will use the name_value_list variable that we're building up here
+        # TODO: name_value_list is probably too specific to Sugar, although we've made it work with other apps
+        #  need a clean way to pass the attrvalues hash to any source adapter cleanly
+        nvlist=make_name_value_list(attrvalues)
+        if @source.source_adapter
+          @source.source_adapter.create(nvlist)
+        else
+          raise ArgumentError,"Need some create code to execute" if @source.createcall.nil?
           callbinding=eval("name_value_list="+nvlist+";"+@source.createcall+";binding",callbinding)
         end
-
       end
     end
 
     # now do the updates
-    if @source.updatecall and @source.updatecall.size>0
-      updates=ObjectValue.find_by_sql("select * from object_values where update_type='update' and source_id="+id)
-      uniqobjs=updates.map {|x|x.object}
-      uniqobjs.uniq!
-      uniqobjs.each do |x|
-        objvals=ObjectValue.find_all_by_object_and_update_type(x,'update')  # this has all the attribute value pairs now
-        attrvalues={}
-        attrvalues["id"]=x  # setting the ID allows it be an update
-        objvals.each do |y|
-          attrvalues[y.attrib]=y.value
-          y.destroy
-        end
-        # now attrvalues has the attribute values needed for the createcall
-        nvlist=make_name_value_list(attrvalues)
+    updates=ObjectValue.find_by_sql("select * from object_values where update_type='update' and source_id="+id)
+    uniqobjs=updates.map {|x|x.object}
+    uniqobjs.uniq!
+    uniqobjs.each do |x|
+      objvals=ObjectValue.find_all_by_object_and_update_type(x,'update')  # this has all the attribute value pairs now
+      attrvalues={}
+      attrvalues["id"]=x  # setting the ID allows it be an update
+      objvals.each do |y|
+        attrvalues[y.attrib]=y.value
+        y.destroy
+      end
+      # now attrvalues has the attribute values needed for the createcall
+      nvlist=make_name_value_list(attrvalues)
+      if @source.source_adapter
+        @source.source_adapter.update(nvlist)
+      else
+        raise ArgumentError,"Need some update code to execute" if @source.updatecall.nil?
         callbinding=eval("name_value_list="+nvlist+";"+@source.updatecall+";binding",callbinding)
       end
     end
 
+
     # now do the deletes
-    if @source.deletecall and @source.deletecall.size>0
-      deletes=ObjectValue.find_by_sql("select * from object_values where update_type='delete' and source_id="+id)
-      uniqobjs=deletes.map {|x|x.object}
-      uniqobjs.uniq!
-      uniqobjs.each do |x|
-        attrvalues={}
-        attrvalues["id"]=x
-        nvlist=make_name_value_list(attrvalues)
+    deletes=ObjectValue.find_by_sql("select * from object_values where update_type='delete' and source_id="+id)
+    uniqobjs=deletes.map {|x|x.object}
+    uniqobjs.uniq!
+    uniqobjs.each do |x|
+      attrvalues={}
+      attrvalues["id"]=x
+      nvlist=make_name_value_list(attrvalues)
+      if @source.source_adapter
+        @source.source_adapter.delete(nvlist)
+      else
+        raise ArgumentError,"Need some delete code to execute" if @source.deletecall.nil?
         callbinding=eval("name_value_list="+nvlist+";"+@source.deletecall+";binding",callbinding)
       end
-      deletes.each do |x|  # get rid of the deletes
-        x.destroy
-      end
     end
-
-    if @source.call
-      # now do the query call
-      p "Executing query call"
+    deletes.each do |x|  # get rid of the deletes
+      x.destroy
+    end
+      
+    # do the query call and sync of records
+    if @source.source_adapter
+      @source.source_adapter.query
+      @source.source_adapter.sync
+    else
       callbinding=eval(@source.call+";binding",callbinding)
-      # now take apart the returned data and fill the object values table
-      p "Executing backend data sync"
       callbinding=eval(@source.sync+";binding",callbinding) if @source.sync
-      ObjectValue.delete_all "(update_type='query') and source_id="+@source.id.to_s
-      ObjectValue.find_by_sql("update object_values set update_type='query' where (update_type='pending' or update_type is null) and source_id="+@source.id.to_s)
     end
+    ObjectValue.delete_all "(update_type='query') and source_id="+@source.id.to_s
+    ObjectValue.find_by_sql("update object_values set update_type='query' where (update_type='pending' or update_type is null) and source_id="+@source.id.to_s)
 
     # now do the logoff
-    if @source.epilog and @source.epilog.size>0
-      callbinding=eval(@source.epilog+";binding",callbinding)
+    if @source.source_adapter
+      @source.source_adapter.logoff
+    else
+      if @source.epilog and @source.epilog.size>0
+        callbinding=eval(@source.epilog+";binding",callbinding)
+      end
     end
 
     @source.refreshtime=Time.new  # keep track of the refresh time to help optimize show queries
--- app/models/source.rb
+++ app/models/source.rb
@@ -1,5 +1,19 @@
 class Source < ActiveRecord::Base
   has_many :object_values
+  attr_accessor :source_adapter
+
+  def before_validate
+    self.initadapter
+  end
+
+  def initadapter
+    #create a source adapter with methods on it if there is a source adapter class identified
+    if self.adapter and self.adapter.size>0
+      @source_adapter=(Object.const_get(self.adapter)).new(self)
+    else # if source_adapter is nil it will
+      @source_adapter=nil
+    end
+  end
 
   # useful to be able to have the source adapter code available for viewing in YAML files
   def save_to_yaml
