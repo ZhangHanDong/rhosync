d03c0a16fa3217672824f329b7276687588e5ac5
lars <larsburgess@gmail.com>
2008-12-16 16:10:35 -0800

    adding uuidtools to required gems

--- config/environment.rb
+++ config/environment.rb
@@ -27,6 +27,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "libxml-ruby", :lib => "xml/libxml"
   config.gem "soap4r", :lib => "soap/mapping"
+  config.gem "uuidtools"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
